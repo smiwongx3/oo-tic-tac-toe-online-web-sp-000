93ce07a2b38dd5d9723fd3655fb00ac9ca88f93c
Samantha Wong <smiwong920@gmail.com>
2019-02-16 00:30:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,58 +1,57 @@
 class TicTacToe
   def initialize
-    @board =[" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end 
-  
-  def WIN_COMBINATIONS =[
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  WIN_COMBINATIONS = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8],
     [0, 3, 6], [1, 4, 7], [2, 5, 8],
     [0, 4, 8], [2, 4, 6]
-    ]
-  end 
-  
-  def display_board 
+  ]
+
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end 
-  
-  def input_to_index(user_input) 
+  end
+
+  def input_to_index(user_input)
     user_input.to_i - 1
-  end 
-  
+  end
+
   def move(index, token)
     @board[index] = token
   end
-  
+
   def position_taken?(index)
     @board[index] != " "
   end
-  
+
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index)
+    !position_taken?(index) && index.between?(0,8)
   end
-  
+
   def turn_count
-    @board.count{|space| space != " " }
+    @board.count{|square| square != " " }
   end
-  
+
   def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+    turn_count.even? ? "X" : "O"
   end
-  
+
   def turn
-    puts "Please enter 1-9:"
+    puts "Please enter a number (1-9):"
     user_input = gets.strip
     index = input_to_index(user_input)
-      if valid_move?(index)
-        token = current_player
-        move(index, token)
-        display_board
-      else
-       turn
-      end
+    if valid_move?(index)
+      token = current_player
+      move(index, token)
+    else
+      turn
+    end
+    display_board
   end
 
   def won?
@@ -62,28 +61,27 @@ class TicTacToe
       end
     end
   end
-  
-  def full? 
-    @board.all?{|space| space != " " }
-  end 
-  
-  def draw? 
+
+  def full?
+    @board.all?{|square| square != " " }
+  end
+
+  def draw?
     full? && !won?
-  end 
-  
-  def over? 
+  end
+
+  def over?
     won? || draw?
-  end 
-  
+  end
+
   def winner
     if combo = won?
       @board[combo[0]]
     end
-  end 
-  
-  def play 
-    turn until over? 
-    puts winner ? "Congratulations #{winner}!" : "Cat's game!"
   end
-    
-end 
+
+  def play
+    turn until over?
+    puts winner ? "Congratulations #{winner}!" : "Cat's Game!"
+  end
+end
